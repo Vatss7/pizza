107b5651c9e422848894e774770c2e662d32e80b
Vatsal <95428759+Vatss7@users.noreply.github.com>
2024-03-09 00:32:15 +0530

    Added Test Website Assets

---
+++ pizza.js
@@ -0,0 +1,28 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "25px",
+  duration: 1000,
+  reset: true,
+});
+
+sr.reveal(
+  `.home-text, .home-img,
+              .about-img, .about-text,
+              .box, .s-box,
+              .btn, .connect-text,
+              .contact-box`,
+  {
+    interval: 200,
+  }
+);
